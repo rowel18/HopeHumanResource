c53e5ccbf475d4850ba8067a8b1962dbcd980536
Rowel <rowel.collado18@gmail.com>
2019-02-26 16:12:33 +0800

    Inserting Data in Database with Simple Validations.

--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -1,13 +1,98 @@
 <template>
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">User's Component</div>
+        <div class="row mt-5">
+          <div class="col-md-12">
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">Responsive Hover Table</h3>
 
-                    <div class="card-body">
-                        I'm an example user component.
+                <div class="card-tools">
+                    <button class="btn btn-success" data-toggle="modal" data-target="#addNew">
+                        Add New
+                        <i class="fas fa-user-plus fa-fw"></i>
+                    </button>
+                </div>
+              </div>
+
+              <div class="card-body table-responsive p-0">
+                <table class="table table-hover">
+                    <tbody>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Type</th>
+                            <th>Modify</th>
+                        </tr>
+                        <tr>
+                            <td>183</td>
+                            <td>John Doe</td>
+                            <td>11-7-2014</td>
+                            <td><span class="tag tag-success">Approved</span></td>
+                            <td>
+                                <a href="#">
+                                    <i class="fas fa-edit blue"></i>
+                                </a>
+                                /
+                                <a href="#">
+                                    <i class="fas fa-trash red"></i>
+                                </a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+              </div>
+              <!-- Card-Body -->
+            </div>
+            <!-- Card -->
+          </div>
+        </div>
+        
+        <!-- Modal -->
+        <div class="modal fade" id="addNew" tabindex="-1" role="dialog" aria-labelledby="addNewLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="addNewLabel">Add New</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
                     </div>
+                    <form @submit.prevent="createUser">
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <input v-model="form.name" type="text" name="name" class="form-control" :class="{ 'is-invalid': form.errors.has('name') }" placeholder="Name">
+                            <has-error :form="form" field="name"></has-error>
+                        </div>
+                        <div class="form-group">
+                            <input v-model="form.email" type="email" name="email" class="form-control" :class="{ 'is-invalid': form.errors.has('email') }" placeholder="Email Address">
+                            <has-error :form="form" field="email"></has-error>
+                        </div>
+                        <div class="form-group">
+                            <textarea v-model="form.bio" type="text" name="bio" id="bio" class="form-control" :class="{ 'is-invalid': form.errors.has('bio') }" placeholder="Short Bio for user (Optional)"></textarea>
+                            <has-error :form="form" field="bio"></has-error>
+                        </div>
+                        <div class="form-group">
+                            <select name="type" v-model="form.type" id="type" class="form-control" :class="{ 'is-invalid': form.errors.has('type') }">
+                                <option value="">Select User Role</option>
+                                <option value="admin">Admin</option>
+                                <option value="user">Standard User</option>
+                                <option value="author">Author</option>
+                            </select>
+                            <has-error :form="form" field="type"></has-error>
+                        </div>
+
+                        <div class="form-group">
+                            <input v-model="form.password" type="password" name="password" id="password"
+                            class="form-control" :class="{ 'is-invalid': form.errors.has('password') }">
+                            <has-error :form="form" field="password"></has-error>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Create</button>
+                    </div>
+                    </form>
                 </div>
             </div>
         </div>
@@ -16,6 +101,28 @@
 
 <script>
     export default {
+        data() {
+            return {
+                // Create a new form instance
+                form: new Form({
+                    name: '',
+                    email: '',
+                    password: '',
+                    type: '',
+                    bio: '',
+                    photo: ''
+
+                })
+            }
+        },
+
+        methods: {
+            createUser() {
+                // Submit the form via a POST request
+                this.form.post('api/user');
+            }
+        },
+
         mounted() {
             console.log('Component mounted.')
         }
